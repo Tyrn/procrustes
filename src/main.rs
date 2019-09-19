4ede6c7bd84e4d31effa495a314621e23cbfc55a
Tyrn <a@b.com>
2019-09-19 16:10:08 +0300

    Stripping numbers from a string

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,29 @@ fn main() {
     println!("Hello, world!");
 }
 
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
+fn str_strip_numbers(s: &str) -> Vec<i64> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\d+").unwrap();
+    }
+    // iterate over all matches
+    RE.find_iter(s)
+        .filter_map(|digits| digits.as_str().parse().ok())  // Filter out numbers out of ixx range.
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn add_two_and_two() {
-        assert_eq!(4, 4);
+    fn stripping_numbers() {
+        assert_eq!(str_strip_numbers("ab11cdd2k.144"), vec![11, 2, 144]);
+        assert!(str_strip_numbers("Ignacio Vazquez-Abrams").is_empty());
     }
 }
