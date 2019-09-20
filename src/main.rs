eba728b2ededc1dc9205de57ae377b036558c754
Tyrn <a@b.com>
2019-09-20 16:31:16 +0300

    Clap started

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,87 @@
-fn main() {
-    println!("Hello, world!");
-}
-
 #[macro_use]
 extern crate lazy_static;
+
+use clap::{Arg, App, SubCommand};
+
+fn main() {
+    let matches = App::new("My Super Program")
+                          .version("1.0")
+                          .author("Kevin K. <kbknapp@gmail.com>")
+                          .about("Does awesome things")
+                          .arg(Arg::with_name("config")
+                               .short("c")
+                               .long("config")
+                               .value_name("FILE")
+                               .help("Sets a custom config file")
+                               .takes_value(true))
+                          .arg(Arg::with_name("INPUT")
+                               .help("Sets the input file to use")
+                               .required(true)
+                               .index(1))
+                          .arg(Arg::with_name("v")
+                               .short("v")
+                               .multiple(true)
+                               .help("Sets the level of verbosity"))
+                          .subcommand(SubCommand::with_name("test")
+                                      .about("controls testing features")
+                                      .version("1.3")
+                                      .author("Someone E. <someone_else@other.com>")
+                                      .arg(Arg::with_name("debug")
+                                          .short("d")
+                                          .help("print debug information verbosely")))
+                          .get_matches();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    let config = matches.value_of("config").unwrap_or("default.conf");
+    println!("Value for config: {}", config);
+
+    // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
+    // required we could have used an 'if let' to conditionally get the value)
+    println!("Using input file: {}", matches.value_of("INPUT").unwrap());
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match matches.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    if let Some(matches) = matches.subcommand_matches("test") {
+        if matches.is_present("debug") {
+            println!("Printing debug info...");
+        } else {
+            println!("Printing normally...");
+        }
+    }
+
+    // more program logic goes here...
+}
 
 use regex::Regex;
 use itertools::join;
 use unicode_segmentation::UnicodeSegmentation;
+use std::path::Path;
+use std::ffi::OsStr;
+
+fn has_ext_of(_path: &str, _ext: &str) -> bool {
+    let p = _path.to_uppercase();
+    let e = _ext.to_uppercase().replace(".", "");
+    Path::new(&p).extension() == Some(OsStr::new(&e))
+}
+
+fn str_strip_numbers(s: &str) -> Vec<i64> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"\d+").unwrap();
+    }
+    // iterate over all matches
+    RE.find_iter(s)
+        .filter_map(|digits| digits.as_str().parse().ok())  // Filter out numbers out of ixx range.
+        .collect()
+}
 
 fn make_initials(authors: &str) -> String {
     lazy_static! {
@@ -29,20 +103,16 @@ fn make_initials(authors: &str) -> String {
     join(_sans_monikers.split(",").map(_by_hyph), ",")
 }
 
-fn str_strip_numbers(s: &str) -> Vec<i64> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(r"\d+").unwrap();
-    }
-    // iterate over all matches
-    RE.find_iter(s)
-        .filter_map(|digits| digits.as_str().parse().ok())  // Filter out numbers out of ixx range.
-        .collect()
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[test]
+    fn checking_ext_of() {
+        assert_eq!(has_ext_of("/alfa/bra.vo/charlie.ogg", "OGG"), true);
+        assert_eq!(has_ext_of("/alfa/bra.vo/charlie.ogg", ".ogg"), true);
+        assert_eq!(has_ext_of("/alfa/bra.vo/charlie.ogg", "mp3"), false);
+    }
     #[test]
     fn stripping_numbers() {
         assert_eq!(str_strip_numbers("ab11cdd2k.144"), vec![11, 2, 144]);
