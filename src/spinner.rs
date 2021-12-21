0a3b4ecf10518caa9dfb3a0f6eee2ef39179e595
Tyrn <orlov@bos-ltd.ru>
2021-12-21 16:52:19 +0300

    PrettySpinner just implemented

--- src/spinner.rs
+++ src/spinner.rs
@@ -1,9 +1,37 @@
+use spinners;
+
 pub trait Spinner {
     fn new() -> Self;
     fn message(&self, line: String);
-    fn stop(&self);
+    fn stop(&mut self);
 }
 
 pub struct PrettySpinner {
+    spinner: Option<spinners::Spinner>,
+}
 
+impl Spinner for PrettySpinner {
+    fn new() -> Self {
+        Self {
+            spinner: Some(spinners::Spinner::new(&spinners::Spinners::Moon, "".into())),
+        }
+    }
+
+    fn message(&self, line: String) {
+        match &self.spinner {
+            Some(spinner) => spinner.message(crate::str_shrink(&(line + crate::BDELIM_ICON), 72)),
+            _ => panic!(
+                "{}PrettySpinner::spinner is already None.{}",
+                crate::BDELIM_ICON,
+                crate::BDELIM_ICON,
+            ),
+        };
+    }
+
+    fn stop(&mut self) {
+        if let Some(spinner) = self.spinner.take() {
+            spinner.stop();
+        }
+        println!("");
+    }
 }
